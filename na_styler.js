7931de998a93601bb2227d3d68c3eefba34cc0fe
Unknown <cgregorio026@west-mec.org>
2019-04-03 16:35:29 -0700

    Final Code Commit
    Ready for review. Inserted a method to change the style rules by clicking on the images at the bottom of the page.

--- na_styler.js
+++ na_styler.js
@@ -39,6 +39,51 @@ function setStyles() {
 
       // creates an element node for the figure element
       var figBox = document.createElement("figure");
+      // sets the id of the figbox element
+      figBox.setAttribute("id", "styleThumbs");
+      // appends the figbox to the div with the id of box
+      document.getElementById("box").appendChild(figBox);
+
+      for (var i = 0; i <= 4; i++) {
+            // creates an image for each of the items in the array.
+            var sheetImg = document.createElement("img");
+            // sets attributes of the image node previously identified
+            sheetImg.setAttribute("src", "na_small_" + i + ".png");
+            sheetImg.setAttribute("alt", "na_style_" + i + ".css");
+
+            // when the sheet image is clicked, it will change the website based on the image presently clicked
+            sheetImg.addEventListener('click', function (e) {
+                  document.getElementById('fancySheet').setAttribute("href", e.target.alt);
+            });
+            // appends the sheet image to the figbox
+            figBox.appendChild(sheetImg);
+      }
+
+      // set up a stylesheet named thumbstyles and appended it to the head
+      var thumbStyles = document.createElement("style");
+      document.head.appendChild(thumbStyles);
+
+      // attatches css styles to the thumbstyles object to dynamically insert css into the document
+      document.styleSheets[document.styleSheets.length - 1].insertRule(
+            "figure#styleThumbs { \
+                  position:absolute; \
+                  left: 0px; \
+                  bottom: 0px; \
+            }", 0);
+
+
+      document.styleSheets[document.styleSheets.length - 1].insertRule(
+            "figure#styleThumbs img { \
+            outline: 1px solid black; \
+            cursor: pointer; \
+            opacity: 0.75; \
+      }", 1);
+
+      document.styleSheets[document.styleSheets.length - 1].insertRule(
+            "figure#styleThumbs img:hover { \
+            outline: 1px solid red; \
+            opacity: 1.0; \
+      }", 2);
 }
 
 function randInt(size) {
